26f7bb610456f75408093c40da07817502d53e45
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 12:43:35 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -14,7 +14,7 @@ export default class FeaturesSection extends React.Component {
                 <h2 className="section__title">{_.get(section, 'title', null)}</h2>
               </div>
               )}
-              <div className="container container--lg pt-10">
+              <div className="container container--lg ">
                 {_.map(_.get(section, 'features', null), (feature, feature_idx) => (
                 <div key={feature_idx} className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': _.get(feature, 'align', null) === 'center', 'align-right': _.get(feature, 'align', null) === 'right'})}>
                   {_.get(feature, 'image', null) && (
