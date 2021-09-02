93b25fdc06931df497024f7c02a9250eb0226c5f
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 12:45:36 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -8,7 +8,7 @@ export default class FeaturesSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <section className="section section--features py-16">
+            <section className="section section--features pt-14">
               {_.get(section, 'title', null) && (
               <div className="container container--md align-center">
                 <h2 className="section__title">{_.get(section, 'title', null)}</h2>
