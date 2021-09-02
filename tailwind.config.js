c4c1da071323f0090bd3ae4f01078036c3010794
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 14:28:55 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
       'display': ['Montserrat', 'Arial', 'sans-serif'],
      },
     colors: {
-        indigo: {
+     indigo: {
         light: '#b3bcf5',
         DEFAULT: '#5c6ac4',
         dark: '#202e78',
-    }
+     }
     },
   },
   variants: {
