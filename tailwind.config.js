cdb0f1cd48066ac0c96fe62d15403684a7dfde58
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 11:36:48 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-       'menu': ['Bebas Neue', 'cursive'],
-      }
+    
     extend: {},
   },
   variants: {
