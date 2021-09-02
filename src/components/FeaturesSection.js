19a840f831ca4b393df4c4dea38077723ba36801
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 12:21:19 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -31,7 +31,7 @@ export default class FeaturesSection extends React.Component {
                     )}
                     {_.get(feature, 'content', null) && (
                     <div className="section__copy">
-                      <span className="text-gray-50" > {markdownify(_.get(feature, 'content', null))}</span>
+                      <span className="text-gray-50 font-display" > {markdownify(_.get(feature, 'content', null))}</span>
                     </div>
                     )}
                     {_.get(feature, 'actions', null) && (
