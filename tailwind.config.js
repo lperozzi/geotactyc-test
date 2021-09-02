661182385afbb1fc76f915cf2a7750b2d61d7c5c
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 14:28:29 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
      },
     colors: {
     indigo: {
-    light: '#b3bcf5',
-    DEFAULT: '#5c6ac4',
-    dark: '#202e78',
+        light: '#b3bcf5',
+        DEFAULT: '#5c6ac4',
+        dark: '#202e78',
     }
   },
   variants: {
