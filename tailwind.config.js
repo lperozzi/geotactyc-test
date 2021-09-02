8a775aece620052492db039c4f759d01bac692f2
Stackbit Code Editor <projects@stackbit.com>
2021-09-02 11:43:24 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-       'sans': ['ui-sans-serif', '', ...],
+       'sans': ['ui-sans-serif', 'Bebas Neue'],
        'serif': ['ui-serif', 'Georgia', ...],
        'mono': ['ui-monospace', 'SFMono-Regular', ...],
        'display': ['Oswald', ...],
