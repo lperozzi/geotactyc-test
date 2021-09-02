4aab6e78619645fc33f2a7fdfd3b903958592a81
Lorenzo Perozzi <lorenzo.perozzi@gmail.com>
2021-09-02 13:57:01 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-       'sans': ['ui-sans-serif', 'Bebas Neue'],
-       'serif': ['ui-sans-serif', 'Montserrat'],
-    //    'mono': ['ui-monospace', 'SFMono-Regular', ...],
-    //    'display': ['Oswald', ...],
-    //    'body': ['"Open Sans"', ...],
-    }
     extend: {},
   },
   variants: {
