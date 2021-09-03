ed683dd06a9783317e081b8b446b34336d7d9b0d
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 06:42:27 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -35,7 +35,7 @@ export default class HeroSection extends React.Component {
                     </div>
                     )}
                     {_.get(section, 'actions', null) && (
-                    <div className="section__actions btn-group font-display text-base text-gray-50">
+                    <div className="section__actions btn-group font-display text-base text-gray-light">
                       <SectionActions {...this.props} actions={_.get(section, 'actions', null)} />
                     </div>
                     
