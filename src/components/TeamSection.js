3a3e6d0eb72db705017d71159d02d450185d6309
Stackbit Code Editor <projects@stackbit.com>
2021-09-07 13:45:05 +0000

    TeamSection.js: changed

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -22,7 +22,7 @@ export default class TeamSection extends React.Component {
                         <div className="card team-member">
                           {person_data.photo && (
                           <figure className="card__media card__media--bottom">
-                            <img src={withPrefix(person_data.photo)} alt={person_data.photo_alt} />
+                            <img className="object-cover shadow-lg rounded-lg" src={withPrefix(person_data.photo)} alt={person_data.photo_alt} />
                           </figure>
                           )}
                           <div className="card__body">
